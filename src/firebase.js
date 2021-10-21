1f1c3149c01e17c79e9e77ac39e4e06fbc262d2f
sajjadViralSquare <sajjad.viralsquare@gmail.com>
2021-10-21 02:34:07 -0400

    firebase file updated

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,7 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: "AIzaSyDlWShmjwLBLgJwM6gJ0NaAgnHwz_SRrBg",
-  // authDomain: "userauth-c5750.firebaseapp.com",
-  // projectId: "userauth-c5750",
-  // storageBucket: "userauth-c5750.appspot.com",
-  // messagingSenderId: "499959172368",
-  // appId: "1:499959172368:web:af0d4bfc18117cd8e8a995"
+  
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
